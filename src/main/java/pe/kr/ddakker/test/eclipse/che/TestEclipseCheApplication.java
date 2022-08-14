f9ba9ebcce58d19b4532f6e33006eb8c97ffc603
ddakker <ddakker@gmail.com>
2022-08-14 13:30:39 +0900

    'init'

---
+++ src/main/java/pe/kr/ddakker/test/eclipse/che/TestEclipseCheApplication.java
@@ -0,0 +1,13 @@
+package pe.kr.ddakker.test.eclipse.che;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestEclipseCheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestEclipseCheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/kr/ddakker/test/eclipse/che/TestEclipseCheApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.kr.ddakker.test.eclipse.che;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestEclipseCheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
